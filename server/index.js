8d98ba38b9311f79f70201c9daba741a1fc01dbc
Luka Sokolov <lukasokolov@gmail.com>
2019-12-24 16:33:40 +0100

    Made a server side of the application

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("42");
+var express = require('express');
+const bodyParser = require('body-parser');
+const setRoutes = require('./routes').setRoutes;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+setRoutes(app);
+
+app.listen(3000);
